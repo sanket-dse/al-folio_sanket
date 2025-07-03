0cc6b2c56544403f2700ef5f10b913ddfc06c6c5
sanket-dse <sankethoude@gmail.com>
2025-07-03 14:02:46 +0000

    Deploying to gh-pages from @ sanket-dse/al-folio_sanket@1af361f73bce179e75cf1d954983ff2dbe625cd0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,41 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/al-folio_sanket/blog/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/al-folio_sanket/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/al-folio_sanket/blog/";
-              },
-            },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/al-folio_sanket/people/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/al-folio_sanket/repositories/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/al-folio_sanket/teaching/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
